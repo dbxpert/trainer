65b3fdf533432097fc88fea9852eac3b0d78a557
sungwoo_chang <sungwoo_chang@tmax.co.kr>
2022-02-14 17:13:32 +0900

    library linking problem solved

--- src/client/builder.py
+++ src/client/builder.py
@@ -7,21 +7,34 @@ class Builder:
     def build(self, debug):
         if os.path.isfile(os.path.join(get_solution_path(), "CMakeLists.txt")):
             os.chdir(get_build_path())
-            if debug:
-                p = subprocess.run(["cmake", "-DCMAKE_BUILD_TYPE=Debug", "-DBUILD_SOLUTION_ONLY=ON", get_project_home_path()])
-                if p.returncode != 0:
-                    raise Exception("Build failed")
-            else:
-                p = subprocess.run(["cmake", "-DCMAKE_BUILD_TYPE=Release", "-DBUILD_SOLUTION_ONLY=ON", get_project_home_path()])
-                if p.returncode != 0:
-                    raise Exception("Build failed")
-            p = subprocess.run(["cmake", "--build", "."])
-            if p.returncode != 0:
-                raise Exception("Build failed")
+            self.cmake(debug)
+            self.compile() 
         else:
             raise Exception("Cannot find CMakeLists.txt")
 
         context.set_debug(debug)
         context.set_compiled(True)
+    
+    def cmake(self, debug):
+        command = ["cmake"]
+        
+        if debug:
+          command.append("-DCMAKE_BUILD_TYPE=Debug")
+        else:
+          command.append("-DCMAKE_BUILD_TYPE=Release")
 
+        command.append("-DBUILD_SOLUTION=ON")
+        command.append("-DBUILD_SERVER=OFF")
+        command.append(get_project_home_path())
+        
+        p = subprocess.run(command)
+        
+        if p.returncode != 0:
+            raise Exception("CMake failed")
+    
+    def compile(self):
+        p = subprocess.run(["cmake", "--build", "."])
+        if p.returncode != 0:
+            raise Exception("Compile failed")
+    
 instance = Builder()
